c563d5a270dd645cdb5f2925a43f73011c0d76c5
shilover <41142363+shilover@users.noreply.github.com>
2018-09-07 01:52:32 +0800

    1
    ansi版编码文件，测试github

---
+++ ANSI编码版(测试解决乱码用)/wegame版/存档备份并开启wegame.bat
@@ -0,0 +1,22 @@
+@echo 备份存档中…………
+@echo.
+@echo.
+@set YYYYmmdd=%date:~0,4%%date:~5,2%%date:~8,2%
+@set sec=%TIME:~3,2%%TIME:~6,2%
+@Set h=%TIME:~0,2%
+@If %h% leq 9 (Set h=0%h:~1,1%)
+@set hhmiss=%h%%sec%
+@set "filename=%YYYYmmdd%_%hhmiss%"
+@rd  .\rail_user_data\BackUpSaveDate\NEW\2000293\ /S /Q 
+@xcopy .\rail_user_data\2000293\*.* .\rail_user_data\BackUpSaveDate\OLD\%filename%\ /S /Q /Y
+@xcopy .\rail_user_data\2000293\*.* .\rail_user_data\BackUpSaveDate\NEW\2000293\ /S /Q /Y
+@echo.
+@echo.
+@echo 备份完成！
+@echo -----------------------------------------
+@echo 本次的备份的存档文件名为：%filename% 
+@echo -----------------------------------------
+@echo.
+@echo.
+@echo.
+@start D:\堡壘之夜\GameLauncher\GameLauncher.exe
